506526c06803348031da2408c411e49875b26308
Treasure Kabareebe <treasurecalm@gmail.com>
2020-09-02 22:48:06 +0300

    Add some validations to measurements

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,3 +1,5 @@
 class Measurement < ApplicationRecord
+  validates :category, :product_id, presence: true
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }, presence: true
   belongs_to :product
 end
--- test/models/measurement_test.rb
+++ test/models/measurement_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class MeasurementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should have a positive quantity" do
+    measurement = measurements(:one)
+    measurement.quantity = -1
+    assert_not measurement.valid?
+  end
 end
