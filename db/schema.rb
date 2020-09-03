d04ac481d5ff1401906a9fad86dfc02f14843485
Treasure Kabareebe <treasurecalm@gmail.com>
2020-09-03 20:18:38 +0300

    Add admin field to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_205635) do
+ActiveRecord::Schema.define(version: 2020_09_03_171421) do
 
   create_table "measurements", force: :cascade do |t|
     t.string "category"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_09_02_205635) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
