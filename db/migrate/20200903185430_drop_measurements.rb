e4cc66d50f9a38c336512c2e356c2d2e36992816
Treasure Kabareebe <treasurecalm@gmail.com>
2020-09-04 17:47:44 +0300

    Clean migrations

--- db/migrate/20200903185430_drop_measurements.rb
+++
@@ -1,5 +0,0 @@
-class DropMeasurements < ActiveRecord::Migration[6.0]
-  def up
-    drop_table :measurements    
-  end
-end
--- db/migrate/20200903192814_drop_product_measurements.rb
+++
@@ -1,5 +0,0 @@
-class DropProductMeasurements < ActiveRecord::Migration[6.0]
-  def up
-    drop_table :product_measurements   
-  end
-end
